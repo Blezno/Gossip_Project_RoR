c16a92acaed35991661a18a0637fcf342cbfe4b5
blezno <gonzaleznovoa.b@protonmail.com>
2022-10-27 21:24:42 +0200

    last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+    Tag.create!(
+        title: Faker::TvShows::HowIMetYourMother.catch_phrase,
+        gossip_id: rand(1..20)
+    )
+end
+
+10.times do
+    City.create!(
+        name: Faker::TvShows::RickAndMorty.location,
+        zip_code: Faker::Address.zip_code
+    )
+end
+
+10.times do
+    User.create!(
+        first_name: Faker::Superhero.name,
+        last_name: Faker::Superhero.suffix,
+        description: Faker::Superhero.power,
+        email: Faker::Superhero.name+"@supermail.com",
+        age: rand(6..80),
+        city_id: rand(1..10)
+    )
+end
+
+20.times do
+    Gossip.create!(
+        title: Faker::TvShows::BrooklynNineNine.character,
+        content: Faker::TvShows::BrooklynNineNine.quote,
+        user_id: rand(1..10),
+        tag_id: rand(1..10)
+    )
+end
+
+
+puts "THANK YOU SO MUCH BESTO FRIENDO"
